58d51efb6047f37cbc0c5830439663aac4ef28e2
chenhongguang01 <chenhongguang01@baidu.com>
2019-01-29 20:49:38 +0800

    add

---
+++ code/basic/test_reshape.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+
+"""
+一个参数为-1时，那么reshape函数会根据另一个参数的维度计算出数组的另外一个shape属性值
+"""
+
+a = np.array([[1, 2, 3], [4, 5, 6]])  
+
+with tf.Session() as sess:
+    #[1 2 3 4 5 6] 一维数组
+    print(sess.run(tf.reshape(a, [-1])))
+
+    #[[1 2 3 4 5 6]] 二维数组
+    print(sess.run(tf.reshape(a, [1, -1])))
+
+    #[[1 2]
+    # [3 4]
+    # [5 6]]
+    print(sess.run(tf.reshape(a, [3, 2])))
+    print(sess.run(tf.reshape(a, [3, -1])))
+
