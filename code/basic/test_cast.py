ed48221aaef0a0dbcd13ab501a0bba4e49f6377c
chenhongguang01 <chenhongguang01@baidu.com>
2019-02-22 23:50:59 +0800

    add

---
+++ code/basic/test_cast.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+
+a = tf.constant([1, 2, 3, 4])
+
+with tf.Session() as sess:
+    #[1. 2. 3. 4.]
+    print(sess.run(tf.cast(a, dtype = tf.float32)))
+
