6f4b428bf40631fb8222fb3530ef670f0b12fa1b
chenhongguang01 <chenhongguang01@baidu.com>
2019-03-20 18:58:29 +0800

    add

---
+++ code/basic/test_exand_dims.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import sys
+import tensorflow as tf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+a = tf.constant([1, 2, 3], tf.int32)
+
+#(1, 3)
+b = tf.expand_dims(a, 0)
+
+# (3, 1)
+c = tf.expand_dims(a, 1) 
+
+with tf.Session() as sess:
+    #[[1 2 3]]
+    print(sess.run(b))
+    #[[1]
+    # [2]
+    # [3]]
+    print(sess.run(c))
+
+
+
