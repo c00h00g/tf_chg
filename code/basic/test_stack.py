bfdb0140b4d2b679c489cb66569d20ae13d4510f
c00h00g <895281773@qq.com>
2019-03-28 17:26:14 +0800

    fix

--- code/basic/test_stack.py
+++ code/basic/test_stack.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import tensorflow as tf
 
 a = tf.constant([1, 2, 3])
@@ -9,9 +11,17 @@ c = tf.stack([a, b], 0)
 #1按照列的方向stack
 d = tf.stack([a, b], 1)
 
+# [a, b]是一个列表
+e = tf.stack([a, b])
+
 with tf.Session() as sess:
     print(sess.run(c))
     print(sess.run(d))
+
+    # 默认按照第一个维度
+    #[[1 2 3]
+    # [4 5 6]]
+    print(sess.run(e))
 
 #result c
 #[[1 2 3]
