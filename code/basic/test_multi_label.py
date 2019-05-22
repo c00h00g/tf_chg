e3ca77509f5320e092a79c40a4225f9ccae3f434
c00h00g <895281773@qq.com>
2019-05-22 21:23:48 +0800

    add

---
+++ code/basic/test_multi_label.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+a = tf.one_hot(1, 4)
+b = tf.one_hot(2, 4)
+c = a + b
+
+with tf.Session() as sess:
+    print(sess.run(a))
+    print(sess.run(b))
+    print(sess.run(c))
