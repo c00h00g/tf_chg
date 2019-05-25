7a8811fbb11b7c6af7403b2884a1c55ceb336076
c00h00g <895281773@qq.com>
2019-05-25 18:23:17 +0800

    add expand dims

--- code/basic/test_expand_dims.py
+++ code/basic/test_expand_dims.py
@@ -10,15 +10,22 @@ a = tf.constant([1, 2, 3], tf.int32)
 b = tf.expand_dims(a, 0)
 
 # (3, 1)
-c = tf.expand_dims(a, 1) 
+c = tf.expand_dims(a, [1]) 
+c1 = tf.expand_dims(a, 1) 
 
 with tf.Session() as sess:
     #[[1 2 3]]
     print(sess.run(b))
+
     #[[1]
     # [2]
     # [3]]
     print(sess.run(c))
+
+    #[[1]
+    # [2]
+    # [3]]
+    print(sess.run(c1))
 
 
 
