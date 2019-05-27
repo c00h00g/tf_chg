811f7a3bda8efb56fe25ec85572f0b598bd51d94
c00h00g <895281773@qq.com>
2019-05-28 00:59:37 +0800

    add tf_range

--- code/basic/test_tf_range.py
+++ code/basic/test_tf_range.py
@@ -3,6 +3,8 @@
 
 import tensorflow as tf
 
+seq_len = 10
+
 # [0 1 2 3 4 5 6 7 8 9]
 # 参数 
 # start
@@ -13,6 +15,11 @@ a = tf.range(0, 10, delta = 1)
 # [0 2 4 6 8]
 b = tf.range(0, 10, delta = 2)
 
+c = a * seq_len
+print c
+
 with tf.Session() as sess:
     print sess.run(a)
     print sess.run(b)
+
+    print sess.run(c)
