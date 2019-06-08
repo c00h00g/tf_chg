03d7221fe244028a234db640a2d32d01ef34f4c6
c00h00g <895281773@qq.com>
2019-06-08 19:06:33 +0800

    add tile

---
+++ code/basic/test_tile.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+
+"""
+纬度扩展
+tile : 平铺的意思
+定义:
+
+tile(
+    input,     #输入
+    multiples, #同一维度上复制的次数
+    name=None)
+
+注意：复制的纬度必须和输入一致
+"""
+
+import tensorflow as tf
+
+a = tf.constant([[[1, 2, 3], [4, 5, 6]]], dtype = tf.int32)
+
+#不进行复制
+b = tf.tile(a, [1, 1, 1])
+
+#[[[1 2 3]
+#  [4 5 6]]
+#
+#  [[1 2 3]
+#  [4 5 6]]]
+c = tf.tile(a, [2, 1, 1])
+
+#[[[1 2 3]
+#  [4 5 6]
+#  [1 2 3]
+#  [4 5 6]]]
+d = tf.tile(a, [1, 2, 1])
+
+#[[[1 2 3 1 2 3]
+#  [4 5 6 4 5 6]]]
+e = tf.tile(a, [1, 1, 2])
+
+#[[[1 2 3 1 2 3]
+#  [4 5 6 4 5 6]]
+#
+#  [[1 2 3 1 2 3]
+#  [4 5 6 4 5 6]]]
+f = tf.tile(a, [2, 1, 2])
+
+with tf.Session() as sess:
+    print(sess.run(b))
+    print('---->')
+
+    print(sess.run(c))
+    print('---->')
+
+    print(sess.run(d))
+    print('---->')
+
+    print(sess.run(e))
+    print('---->')
+
+    print(sess.run(f))
+    print('---->')
