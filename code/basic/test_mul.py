d7d887840195d15594661c27d8fead4d2826c267
chenhongguang01 <chenhongguang01@baidu.com>
2019-06-12 20:44:09 +0800

    add

--- code/basic/test_mul.py
+++ code/basic/test_mul.py
@@ -33,8 +33,8 @@ with tf.Session() as sess:
     #[1 4 9]
     print(sess.run(tf.multiply(a, b)))
 
-    #[1 4 9]
-    #矩阵点乘可以直接用乘号
+    # [1 4 9]
+    # 矩阵点乘可以直接用乘号
     print(sess.run(a * b))
 
     #[[14]]
