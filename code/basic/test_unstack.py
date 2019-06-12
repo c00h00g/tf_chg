1c7fed802ab28209203006d1bf94a72c14268d9d
chenhongguang01 <chenhongguang01@baidu.com>
2019-06-12 15:21:59 +0800

    add

--- code/basic/test_unstack.py
+++ code/basic/test_unstack.py
@@ -1,12 +1,23 @@
+# -*- coding:utf-8 -*-
+
+"""
+按照某个纬度拆分成数组
+"""
+
 import tensorflow as tf
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
+# 2 * 3 
 a = tf.constant([[1, 2, 3], [4, 5, 6]])
+
+# 按照第一个纬度拆分
 b = tf.unstack(a, axis = 0)
+
 c = b[0]
 d = b[1]
 
+# 按照第二个纬度拆分
 e = tf.unstack(a, axis = 1)
 
 with tf.Session() as sess:
@@ -17,7 +28,6 @@ with tf.Session() as sess:
     #[4 5 6]
     print(sess.run(d))
 
-    #list瀵硅薄
     #[array([1, 4], dtype=int32), array([2, 5], dtype=int32), array([3, 6], dtype=int32)]
     print(sess.run(e))
     
