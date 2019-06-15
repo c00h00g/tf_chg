56313a369f89985be51ba49193da3998db32d752
c00h00g <895281773@qq.com>
2019-06-15 16:03:40 +0800

    add

--- code/basic/lstm.py
+++ code/basic/lstm.py
@@ -10,7 +10,6 @@ lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units = 128)
 inputs = tf.placeholder(np.float32, shape = (32, 100))
 h0 = lstm_cell.zero_state(32, np.float32)
 
-#如何单步调用
 output, h1 = lstm_cell.__call__(inputs, h0)
 
 # lstm's output contains two states cell_state & hidden state
