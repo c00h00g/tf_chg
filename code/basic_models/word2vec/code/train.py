18cee390edb55c2b4bca231f4b2dd973aa65e271
chenhongguang01 <chenhongguang01@baidu.com>
2019-06-19 18:08:00 +0800

    fix train.py

--- code/basic_models/word2vec/code/train.py
+++ code/basic_models/word2vec/code/train.py
@@ -1,17 +1,66 @@
-# -*- coding:utf-8 -*-
+#t -*- coding:utf-8 -*-
 
+import sys
+import os
 import tensorflow as tf
+import numpy as np
 
-epochs = 1
-batch_size = 128
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+def load_data():
+    """
+    load train data
+    """
+    all_data = []
+    with open('input_data') as f:
+        for line in f.readlines():
+            line = line.rstrip()
+            line_sp = line.split(' ')
+            all_data.append(line_sp)
+    return all_data
+
+def get_batch_data(data_list, start_idx, batch_size):
+    """
+    get a batch data
+    """
+    data_len = len(data_list)
+    next_start = -1
+    key_before = ""
+    # center word count
+    num = 0
+    res_list = []
+
+    i = -1
+    for i in range(start_idx, data_len):
+        key = data_list[i][0]
+        if key != key_before:
+            if num == batch_size:
+                next_start = i
+                break
+            res_list.append(data_list[i])
+            key_before = key
+            num += 1
+        else:
+            res_list.append(data_list[i])
+
+    if i == -1 or i == data_len - 1:
+        next_start = -1
+
+    return next_start, np.array(res_list)
+
+################### load data #####################
+all_data = load_data()
+
+epochs = 10
+batch_size = 100
 emb_size = 100
-vocab_size = 1000
+vocab_size = 19188
 
-x = tf.placeholder(tf.int32, [None, 6])
+x = tf.placeholder(tf.int32, [None, 7])
 
 pos_u = tf.slice(x, [0, 0], [-1, 1])
 pos_v = tf.slice(x, [0, 1], [-1, 1])
-neg_v = tf.slice(x, [0, 2], [-1, 4])
+neg_v = tf.slice(x, [0, 2], [-1, 5])
 
 pos_u = tf.one_hot(pos_u, depth = vocab_size, dtype = tf.float32)
 pos_v = tf.one_hot(pos_v, depth = vocab_size, dtype = tf.float32)
@@ -32,7 +81,7 @@ v_emb = tf.Variable(tf.random_normal([vocab_size, emb_size]))
 # trans to emb
 pos_u_emb = tf.reshape(tf.matmul(pos_u, u_emb), [-1, 1, 100])
 pos_v_emb = tf.reshape(tf.matmul(pos_v, v_emb),  [-1, 1, 100])
-neg_v_emb = tf.reshape(tf.matmul(neg_v, v_emb), [-1, 4, 100])
+neg_v_emb = tf.reshape(tf.matmul(neg_v, v_emb), [-1, 5, 100])
 
 print(pos_u_emb)
 print(pos_v_emb)
@@ -50,7 +99,28 @@ print(pos_loss)
 print(neg_loss)
 print(loss)
 
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(epochs):
+        start_idx = 0
+        num = 0
+        while True:
+            new_idx, batch_data = get_batch_data(all_data, start_idx, batch_size)
+            if new_idx == -1:
+                break
+            start_idx = new_idx
+
+            _, loss_value = sess.run([train_op, loss], feed_dict = {x : batch_data})
+
+            num += 1
+
+            if num % 100 == 0:
+                print('loss is : ', loss_value)
 
 
 
-#with tf.Session() as sess:
+
+
