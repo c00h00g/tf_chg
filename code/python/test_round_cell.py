fd2c00e8ab2cbe62be9de02a7522b1dcdaa121e0
c00h00g <895281773@qq.com>
2019-06-21 11:39:53 +0800

    add

--- code/python/test_round_cell.py
+++ code/python/test_round_cell.py
@@ -5,12 +5,12 @@ import numpy as np
 
 a = [1.1, 4.5, 5.6]
 
-#最近的偶数
+#鏈�杩戠殑鍋舵暟
 #For values exactly halfway between rounded decimal values, NumPy rounds to the nearest even value. Thus 1.5 and 2.5 round to 2.0, -0.5 and 0.5 round to 0.0, etc.
 print np.round(a)
 
-#向上取整
+#鍚戜笂鍙栨暣
 print np.ceil(a)
 
-#向下取整
+#鍚戜笅鍙栨暣
 print np.floor(a)
