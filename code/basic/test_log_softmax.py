68988e7f35a8d13b7295c27ccecce369ad08c733
c00h00g <895281773@qq.com>
2019-06-25 15:00:54 +0800

    add

--- code/basic/test_log_softmax.py
+++ code/basic/test_log_softmax.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# tf.nn.log_soft的用法主要是在计算交叉熵的时候，-y * log(f(x)) - (1 - y) * log(1 - f(x))
+# tf.nn.log_softmax的用法主要是在计算交叉熵的时候，-y * log(f(x)) - (1 - y) * log(1 - f(x))
 
 import tensorflow as tf
 import numpy as np
