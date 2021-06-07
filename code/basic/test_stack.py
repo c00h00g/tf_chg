996a7d2ee11ddb569db3e50d9a56c48a12ef3086
c00h00g <895281773@qq.com>
2021-06-07 22:37:15 +0800

    fix test_stack

--- code/basic/test_stack.py
+++ code/basic/test_stack.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 
 a = tf.constant([1, 2, 3])
 b = tf.constant([4, 5, 6])
@@ -14,14 +15,17 @@ d = tf.stack([a, b], 1)
 # [a, b]是一个列表
 e = tf.stack([a, b])
 
-with tf.Session() as sess:
-    print(sess.run(c))
-    print(sess.run(d))
+#with tf.Session() as sess:
+#with tf.compat.v1.Session() as sess:
 
-    # 默认按照第一个维度
-    #[[1 2 3]
-    # [4 5 6]]
-    print(sess.run(e))
+sess= tf.compat.v1.Session()
+print(sess.run(c))
+print(sess.run(d))
+
+# 默认按照第一个维度
+#[[1 2 3]
+# [4 5 6]]
+print(sess.run(e))
 
 #result c
 #[[1 2 3]
