f201c76c46b79d775653ecc1c9baca296404286f
c00h00g <895281773@qq.com>
2021-06-08 23:06:25 +0800

    add

---
+++ code/basic/test_logloss.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import tensorflow as tf
+
+logits = tf.constant([1, 2, 3], dtype = tf.float32)
+labels = [0, 1, 0]
+
+
+logits_softmax = tf.nn.softmax(logits)
+loss1 = -tf.reduce_sum(tf.log(logits_softmax) * labels)
+
+loss2 = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+
+
+with tf.Session() as sess:
+    print(sess.run(loss1))
+    print(sess.run(loss2))
