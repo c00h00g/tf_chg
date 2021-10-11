eee4eba4a3cafbeb8644cf8abd302f78b411f5e3
c00h00g <895281773@qq.com>
2021-10-11 21:09:57 +0800

    add

--- code/basic/test_range.py
+++ code/basic/test_range.py
@@ -2,10 +2,20 @@
 import tensorflow as tf
 
 a = tf.range(0, 5, dtype = tf.int32)
+b = a * 3
+c = tf.reshape(b, [-1, 1])
 
 with tf.Session() as sess:
     print(sess.run(a))
+    print(sess.run(b))
+    print(sess.run(c))
 
 
 # output
 # [0 1 2 3 4]
+# [ 0  3  6  9 12]
+# [[ 0]
+#  [ 3]
+#  [ 6]
+#  [ 9]
+#  [12]]
