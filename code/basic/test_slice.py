9313eb48c461ad8a1944a84f2bfb5a797a2ff5c4
c00h00g <895281773@qq.com>
2021-10-13 00:21:21 +0800

    add test slice

--- code/basic/test_slice.py
+++ code/basic/test_slice.py
@@ -1,44 +1,22 @@
-import sys
-import os
-import numpy as np
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
 
 import tensorflow as tf
 
-t = tf.constant([[[1, 1, 1], [2, 2, 2]],
-                 [[3, 3, 3], [4, 4, 4]],
-                 [[5, 5, 5], [6, 6, 6]]])
+a = tf.constant([[1, 2, 3], [4, 5, 6]])
+b = a[:, 0:2]
+c = a[:, 2:]
 
-#Tensor("Const:0", shape=(3, 2, 3), dtype=int32)
-print(t)
-
-#Tensor("Slice:0", shape=(1, 2, 3), dtype=int32)
-s_res1 = tf.slice(t, [1, 0, 0], [1, 2, 3])
-s_res2 = tf.slice(t, [1, 0, 0], [2, 2, 2])
-s_res3 = tf.slice(t, [1, 0, 0], [1, 2, -1])
+#print(c)
 
 with tf.Session() as sess:
-    #shape: (1, 2, 3)
-    #[[[3 3 3]
-    #  [4 4 4]]]
-    print(s_res1.shape)
-    print(sess.run(s_res1))
+    print(sess.run(a))
+    print(sess.run(b))
+    print(sess.run(c))
 
-    #shape: (2, 2, 2)
-    #[[[3 3]
-    #  [4 4]]
-    # [[5 5]
-    #  [6 6]]]
-    print(s_res2.shape)
-    print(sess.run(s_res2))
-
-    #[[[3 3 3]
-    #  [4 4 4]]]
-    print(sess.run(s_res3))
-
-    # False
-    print(s_res1 == s_res3)
-
-    #[[[ True  True  True]
-    #  [ True  True  True]]]
-    print(sess.run(tf.equal(s_res1, s_res3)))
+#outputs
+#[[1 2 3]
+# [4 5 6]]
+#[[1 2]
+# [4 5]]
+#[[3]
+# [6]]
