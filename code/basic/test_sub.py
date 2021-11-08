2fbc17cacd76b56b6ac6d62b559031eda4439c8d
c00h00g <895281773@qq.com>
2021-11-08 23:32:48 +0800

    add subtract

--- code/basic/test_sub.py
+++ code/basic/test_sub.py
@@ -3,10 +3,16 @@ import tensorflow as tf
 a = tf.constant([[1, 2, 3], [4, 5, 6]])
 b = tf.constant([[1, 1, 1], [1, 1, 1]])
 
-#[[0 1 2]
-# [3 4 5]]
 c = tf.subtract(a, b)
+d = tf.subtract(a, 3)
 
 
 with tf.Session() as sess:
     print(sess.run(c))
+    print(sess.run(d))
+
+# outputs
+#[[0 1 2]
+# [3 4 5]]
+#[[-2 -1  0]
+# [ 1  2  3]]
