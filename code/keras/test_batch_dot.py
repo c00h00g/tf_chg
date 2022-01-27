2be0648bbca5a9930f1727ce38ef9f18b71670bb
c00h00g <895281773@qq.com>
2022-01-27 22:47:36 +0800

    add dot

--- code/keras/test_batch_dot.py
+++ code/keras/test_batch_dot.py
@@ -5,30 +5,83 @@ import tensorflow as tf
 import numpy as np
 
 
-a = np.random.randint(0, 3, size=(3, 2))
-a_t = tf.convert_to_tensor(a)
+def test_1(sess):
+    #ouptus
+    #[[0 0]
+    # [1 1]
+    # [0 1]]
+    #[[0 1]
+    # [1 2]
+    # [2 1]]
+    #[[0]
+    # [3]
+    # [1]]
+    # batch保持不变, 第二维进行x * x^T 维度是1
+    a = np.random.randint(0, 3, size=(3, 2))
+    a_t = tf.convert_to_tensor(a)
 
-b = np.random.randint(0, 3, size=(3, 2))
-b_t = tf.convert_to_tensor(b)
+    b = np.random.randint(0, 3, size=(3, 2))
+    b_t = tf.convert_to_tensor(b)
 
-# a_t : (3, 2)
-# b_t : (3, 2)
-# output: 3 * 1
-c = tf.keras.backend.batch_dot(a_t, b_t, axes=1)
-
-
-with tf.Session() as sess:
+    # a_t : (3, 2)
+    # b_t : (3, 2)
+    # output: 3 * 1
+    c = tf.keras.backend.batch_dot(a_t, b_t, axes=1)
     print(sess.run(a_t))
     print(sess.run(b_t))
     print(sess.run(c))
-    
-#ouptus
-#[[0 0]
-# [1 1]
-# [0 1]]
-#[[0 1]
-# [1 2]
-# [2 1]]
-#[[0]
-# [3]
-# [1]]
+
+def test_2(sess):
+    x_batch = tf.convert_to_tensor(np.random.randint(0, 3, size=(2, 2, 1)))
+    y_batch = tf.convert_to_tensor(np.random.randint(0, 3, size=(2, 3, 2)))
+    xy_batch_dot = tf.keras.backend.batch_dot(x_batch, y_batch, axes=(1, 2))
+    print(sess.run(x_batch))
+    print(sess.run(y_batch))
+    print(sess.run(xy_batch_dot))
+
+
+
+with tf.Session() as sess:
+   print('chg test_1 result is ---------------------------->') 
+   test_1(sess)
+   print('chg test_2 result is ---------------------------->') 
+   test_2(sess)
+
+
+"""
+chg test_1 result is ---------------------------->
+[[1 0]
+ [2 2]
+ [2 1]]
+[[0 2]
+ [1 1]
+ [1 0]]
+[[0]
+ [4]
+ [2]]
+
+chg test_2 result is ---------------------------->
+[[[2]
+  [1]]
+
+ [[0]
+  [0]]]
+
+[[[0 0]
+  [0 0]
+  [2 1]]
+
+ [[0 2]
+  [0 2]
+  [2 1]]]
+[[[0 0 5]]
+
+ [[0 0 0]]]
+"""
+
+
+
+
+
+
+
--- code/keras/test_dot.py
+++ code/keras/test_dot.py
@@ -1,37 +1,46 @@
 # -*- coding:utf-8 -*-
 
+"""
+
+"""
+
 
 import tensorflow as tf
 
-a = tf.constant([[1, 2, 3], [4, 5, 6]])
-# (2,1,3)
-a = tf.expand_dims(a, axis=1)
 
-b = tf.constant([1, 1, 1])
-#(3,1)
-b = tf.expand_dims(b, axis=1)
+def test_1(sess):
+    """
+    [[[1 2 3]]
 
-# (2,1,1)
-c = tf.keras.backend.dot(a, b)
+     [[4 5 6]]]
 
+    [[1]
+     [1]
+     [1]]
 
-with tf.Session() as sess:
+    [[[ 6]]
+
+     [[15]]]
+     两个tensor的矩阵相乘
+    """
+    a = tf.constant([[1, 2, 3], [4, 5, 6]])
+    # (2,1,3)
+    a = tf.expand_dims(a, axis=1)
+
+    b = tf.constant([1, 1, 1])
+    #(3,1)
+    b = tf.expand_dims(b, axis=1)
+
+    # (2,1,1)
+    c = tf.keras.backend.dot(a, b)
+
     print(sess.run(a))
     print(sess.run(b))
     print(sess.run(c))
 
 
 
-"""
-[[[1 2 3]]
+with tf.Session() as sess:
+    print("chg test_1 is -------------->")
+    test_1(sess)
 
- [[4 5 6]]]
-
-[[1]
- [1]
- [1]]
-
-[[[ 6]]
-
- [[15]]]
-"""
