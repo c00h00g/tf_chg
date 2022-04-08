134b3540989254541b716c4601c0731bc8f0e220
c00h00g <895281773@qq.com>
2022-04-08 22:39:13 +0800

    add

--- code/basic/test_logloss.py
+++ code/basic/test_logloss.py
@@ -2,19 +2,22 @@
 
 import tensorflow as tf
 
-logits = tf.constant([1, 2, 3], dtype = tf.float32)
-labels = [0, 1, 0]
+logits = tf.constant([[1, 2], [3, 4]], dtype = tf.float32)
+labels = tf.constant([0, 1], dtype = tf.int32)
+weights = tf.constant([1, 2], dtype=tf.float32)
 
 
 logits_softmax = tf.nn.softmax(logits)
-loss1 = -tf.reduce_sum(tf.log(logits_softmax) * labels)
+labels_new = tf.one_hot(labels, depth=2, dtype=tf.float32)
+loss1 = -tf.reduce_mean(tf.reduce_sum(tf.log(logits_softmax) * labels_new, axis=-1))
 
 
 # loss2会自动做softmax操作
-loss2 = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+loss2 = tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels)
 
 
 with tf.Session() as sess:
+    print(sess.run(labels_new))
     print(sess.run(loss1))
     print(sess.run(loss2))
 
