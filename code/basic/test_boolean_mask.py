92311040fb4685888725d3d68a519041ff8fef42
c00h00g <895281773@qq.com>
2023-05-24 23:20:05 +0800

    boolean mask

--- code/basic/test_boolean_mask.py
+++ code/basic/test_boolean_mask.py
@@ -1,16 +1,25 @@
+# -*- coding:utf-8 -*-
+
+"""
+被mask的元素直接删除
+"""
 
 import tensorflow as tf
 
-a = tf.constant([1, 2, 3], dtype = tf.float32)
-b = tf.constant([True, False, True], dtype=tf.bool)
+a = tf.constant([[2, 3], [4, 5]], dtype = tf.float32)
+b = tf.constant([True, False], dtype=tf.bool)
 
-c = tf.boolean_mask(a, b)
+c = tf.boolean_mask(a, b, axis=0)
+d = tf.boolean_mask(a, b, axis=1)
 
+"""
+[[2. 3.]]
+
+[[2.]
+ [4.]]
+"""
 
 with tf.Session() as sess:
     print(sess.run(c))
+    print(sess.run(d))
 
-
-"""
-[1. 3.]
-"""
